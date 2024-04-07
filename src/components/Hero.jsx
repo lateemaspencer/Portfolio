45bd3ccf28d42d820f1dec1cd3626c9113c040e9
Lateema Spencer <spencer.lateema@gmail.com>
2024-04-06 21:12:00 -0400

    updated work status

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,13 +17,30 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Lateema</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I build beautiful and functional
+            I build beautiful and functional &nbsp;
             <br className="sm:block hidden" />
             websites from front to back
           </p>
         </div>
       </div>
       <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop',
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
